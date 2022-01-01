ae699c722f3e144d870ac4f23232a113651a9537
Khanh Nguyen <khanhnt92@gmail.com>
2022-01-01 23:46:43 +0700

    abc

--- Enofibom/Form1.Designer.cs
+++ Enofibom/Form1.Designer.cs
@@ -30,10 +30,6 @@ namespace Enofibom
         private void InitializeComponent()
         {
             this.mapControl = new GMap.NET.WindowsForms.GMapControl();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.jrButton1 = new Enofibom.JRButton();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mapControl
@@ -65,62 +61,17 @@ namespace Enofibom
             this.mapControl.TabIndex = 0;
             this.mapControl.Zoom = 0D;
             // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.jrButton1);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(921, 13);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(368, 599);
-            this.panel1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(15, 20);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Addmarker";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // jrButton1
-            // 
-            this.jrButton1.BackColor = System.Drawing.Color.GreenYellow;
-            this.jrButton1.BackgroundColor = System.Drawing.Color.GreenYellow;
-            this.jrButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.jrButton1.BorderRadius = 20;
-            this.jrButton1.BorderSize = 0;
-            this.jrButton1.FlatAppearance.BorderSize = 0;
-            this.jrButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.jrButton1.ForeColor = System.Drawing.Color.White;
-            this.jrButton1.Location = new System.Drawing.Point(152, 114);
-            this.jrButton1.Name = "jrButton1";
-            this.jrButton1.Size = new System.Drawing.Size(150, 40);
-            this.jrButton1.TabIndex = 1;
-            this.jrButton1.Text = "jrButton1";
-            this.jrButton1.TextColor = System.Drawing.Color.White;
-            this.jrButton1.UseVisualStyleBackColor = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1301, 624);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.mapControl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -128,9 +79,6 @@ namespace Enofibom
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl mapControl;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private JRButton jrButton1;
     }
 }
 
