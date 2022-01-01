28af64cc6b5b0a4d7543c445a002ece7a9aefb67
Khanh Nguyen <khanhnt92@gmail.com>
2022-01-01 21:37:55 +0700

    Add project files.

---
+++ Enofibom/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Enofibom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.jrButton1 = new Enofibom.JRButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mapControl
+            // 
+            this.mapControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapControl.Bearing = 0F;
+            this.mapControl.CanDragMap = true;
+            this.mapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this.mapControl.GrayScaleMode = false;
+            this.mapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.mapControl.LevelsKeepInMemory = 5;
+            this.mapControl.Location = new System.Drawing.Point(12, 12);
+            this.mapControl.MarkersEnabled = true;
+            this.mapControl.MaxZoom = 2;
+            this.mapControl.MinZoom = 2;
+            this.mapControl.MouseWheelZoomEnabled = true;
+            this.mapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.mapControl.Name = "mapControl";
+            this.mapControl.NegativeMode = false;
+            this.mapControl.PolygonsEnabled = true;
+            this.mapControl.RetryLoadTile = 0;
+            this.mapControl.RoutesEnabled = true;
+            this.mapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.mapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.mapControl.ShowTileGridLines = false;
+            this.mapControl.Size = new System.Drawing.Size(902, 600);
+            this.mapControl.TabIndex = 0;
+            this.mapControl.Zoom = 0D;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.jrButton1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(921, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 599);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(15, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Addmarker";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // jrButton1
+            // 
+            this.jrButton1.BackColor = System.Drawing.Color.GreenYellow;
+            this.jrButton1.BackgroundColor = System.Drawing.Color.GreenYellow;
+            this.jrButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.jrButton1.BorderRadius = 20;
+            this.jrButton1.BorderSize = 0;
+            this.jrButton1.FlatAppearance.BorderSize = 0;
+            this.jrButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.jrButton1.ForeColor = System.Drawing.Color.White;
+            this.jrButton1.Location = new System.Drawing.Point(152, 114);
+            this.jrButton1.Name = "jrButton1";
+            this.jrButton1.Size = new System.Drawing.Size(150, 40);
+            this.jrButton1.TabIndex = 1;
+            this.jrButton1.Text = "jrButton1";
+            this.jrButton1.TextColor = System.Drawing.Color.White;
+            this.jrButton1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1301, 624);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mapControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl mapControl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private JRButton jrButton1;
+    }
+}
+
---
+++ Enofibom/Form1.cs
@@ -0,0 +1,182 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Device.Location;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enofibom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mapControl.MapProvider = GMapProviders.GoogleMap;
+            var point = new PointLatLng(21.020440, 105.843650);
+            
+            mapControl.Position = point;
+
+            mapControl.DragButton = MouseButtons.Left;
+
+            mapControl.MinZoom = 5; 
+            mapControl.MaxZoom = 22;
+            mapControl.Zoom = 13;
+            mapControl.ShowCenter = false;
+
+
+            
+            //int segments = 500;
+
+            //List<PointLatLng> gpollist = new List<PointLatLng>();
+
+            //for (int i = 0; i < segments; i++)
+            //    gpollist.Add(FindPointAtDistanceFrom(point, i, 1130.89 / 1000));
+
+            //GMapPolygon gpol = new GMapPolygon(gpollist, "pol");
+            //GMapOverlay markers = new GMapOverlay("ABCDS");
+
+            //markers.Polygons.Add(gpol);
+            //mapControl.Overlays.Add(markers);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var point = new PointLatLng(21.020440, 105.843650);
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.red_dot);
+            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            marker.ToolTipText = "dfdfdf" + Environment.NewLine + "yriryri";
+            marker.IsVisible = true;
+
+
+
+
+            var point2 = new PointLatLng(21.016830, 105.855760);
+            var markerLoduc = new GMarkerGoogle(point2, GMarkerGoogleType.blue_dot);
+            markerLoduc.IsVisible = true;
+            markerLoduc.ToolTipMode = MarkerTooltipMode.Always;
+            markerLoduc.ToolTipText = "Lò Đúc" + Environment.NewLine + "Hà Nội";
+
+
+            GMapOverlay markers = new GMapOverlay("ABCDS");
+            //GMapOverlay overlayloduc = new GMapOverlay("ABCDfdfdfdS");
+            mapControl.Overlays.Add(markers);
+            //mapControl.Overlays.Add(overlayloduc);
+
+
+            markers.Markers.Add(marker);
+            markers.Markers.Add(markerLoduc);
+
+
+            //PointLatLng point = new PointLatLng(lat, lon);
+           
+            var polygon1 = CreateCircle(point.Lat, point.Lng, 500, 2);
+            var polygon2 = CreateCircle(point2.Lat, point2.Lng, 1200, 2);
+
+            markers.Polygons.Add(polygon1);
+            markers.Polygons.Add(polygon2);
+            
+
+        }
+
+        private void RefreshMap()
+        {
+            //mapControl.Zoom = -13;
+            //mapControl.Zoom = 13;
+            mapControl.Refresh();
+        }
+
+        private GMapPolygon CreateCircle(Double lat, Double lon, double radius, int ColorIndex)
+        {
+            PointLatLng point = new PointLatLng(lat, lon);
+            int segments = 1080;
+
+            List<PointLatLng> gpollist = new List<PointLatLng>();
+
+            for (int i = 0; i < segments; i++)
+            {
+                gpollist.Add(FindPointAtDistanceFrom(point, i * (Math.PI / 180), radius / 1000));
+            }
+
+            GMapPolygon polygon = new GMapPolygon(gpollist, "Circle");
+            switch (ColorIndex)
+            {
+
+                case 1:
+                    polygon.Fill = new SolidBrush(Color.FromArgb(65, Color.Red));
+                    break;
+                case 2:
+                    polygon.Fill = new SolidBrush(Color.FromArgb(65, Color.Gray));
+                    break;
+                case 3:
+                    polygon.Fill = new SolidBrush(Color.FromArgb(20, Color.Aqua));
+                    break;
+                default:
+                    MessageBox.Show("No search zone found!");
+                    break;
+            }
+
+
+            polygon.Stroke = new Pen(Color.Gray, 1);
+            return polygon;
+        }
+
+
+        public static GMap.NET.PointLatLng FindPointAtDistanceFrom(GMap.NET.PointLatLng startPoint, double initialBearingRadians, double distanceKilometres)
+        {
+            const double radiusEarthKilometres = 6371.01;
+            var distRatio = distanceKilometres / radiusEarthKilometres;
+            var distRatioSine = Math.Sin(distRatio);
+            var distRatioCosine = Math.Cos(distRatio);
+
+            var startLatRad = DegreesToRadians(startPoint.Lat);
+            var startLonRad = DegreesToRadians(startPoint.Lng);
+
+            var startLatCos = Math.Cos(startLatRad);
+            var startLatSin = Math.Sin(startLatRad);
+
+            var endLatRads = Math.Asin((startLatSin * distRatioCosine) + (startLatCos * distRatioSine * Math.Cos(initialBearingRadians)));
+            var endLonRads = startLonRad + Math.Atan2(Math.Sin(initialBearingRadians) * distRatioSine * startLatCos, distRatioCosine - startLatSin * Math.Sin(endLatRads));
+
+            return new GMap.NET.PointLatLng(RadiansToDegrees(endLatRads), RadiansToDegrees(endLonRads));
+        }
+
+        public static double DegreesToRadians(double degrees)
+        {
+            const double degToRadFactor = Math.PI / 180;
+            return degrees * degToRadFactor;
+        }
+
+        public static double RadiansToDegrees(double radians)
+        {
+            const double radToDegFactor = 180 / Math.PI;
+            return radians * radToDegFactor;
+        }
+
+
+
+        public static double DistanceTwoPoint(double startLat, double startLong, double endLat, double endLong)
+        {
+
+            var startPoint = new GeoCoordinate(startLat, startLong);
+            var endPoint = new GeoCoordinate(endLat, endLong);
+
+            return startPoint.GetDistanceTo(endPoint);
+        }
+
+    }
+}
